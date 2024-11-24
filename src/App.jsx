92d9ebc26cb541492092b7f9b051ac667ce41b63
usaid ahmed <usaidahmed61524.com>
2024-11-24 20:40:58 +0500

    changes in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/style.css';
 import Header from "./components/Header";
